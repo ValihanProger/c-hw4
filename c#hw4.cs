80b77545d816524a4dbb027239b2f6a68acedaf0
Valihan <valihanburhanov@yandex.ru>
2023-01-24 11:26:25 +0300

    ratatatata

--- c#hw4.cs
+++ c#hw4.cs
@@ -1,23 +1,36 @@
-﻿// Задача 25: Напишите цикл, который принимает 
-// на вход два числа (A и B) и возводит число 
-// A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+﻿// void Arr(int count)
+// {
+//     int[] array;
+//     if (count <= 8)
+//     {
+//         array = new int[count];
+//         for (int i = 0; i < array.Length; i++)
+//         {
+//             array[i] = Convert.ToInt32(Console.ReadLine());
+//         }
+//         Console.WriteLine($"[{string.Join(",", array)}]");
 
-int Square(int a, int b)
+//     }
+
+//     else
+//     {
+//         Console.Write("Максимальное колличество элементов 8.");
+//     }
+//     return array;
+// }
+
+int Arr(int enterNumber, int[] array)
 {
-    int c = 0;
-    int k = 0;
-    for (int i = 0; k <= b; i++)
+    for (int i = 0; i < enterNumber; i++)
     {
-        c = a*b;
-        k++;
-        Console.WriteLine(c);
+        array[i] = new Random().Next(enterNumber + 1);
+        Console.Write(array[i] + "; ");
     }
-
-
-    return c;
+    return array;
+    Console.WriteLine();
 }
-
-
-Square(3,5);
+Console.WriteLine("Будет создан массив длиной A в диапвзоне 0-A");
+Console.Write("Введите любое число (A): ");
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[enterNumber];
+Arr(8, array);
