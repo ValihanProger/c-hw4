5fffd1dd7b38f8b69769f88ae8ed5d8266806d67
Valihan <valihanburhanov@yandex.ru>
2023-01-24 10:37:44 +0300

    не решил до конца

--- c#hw4.cs
+++ c#hw4.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает 
+// на вход два числа (A и B) и возводит число 
+// A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Square(int a, int b)
+{
+    int c = 0;
+    int k = 0;
+    for (int i = 0; k <= b; i++)
+    {
+        c = a*b;
+        k++;
+        Console.WriteLine(c);
+    }
+
+
+    return c;
+}
+
+
+Square(3,5);
