c0d26e81c478cc5e7b0d525d5c5e9e32b2252caf
Valihan <valihanburhanov@yandex.ru>
2023-01-24 11:28:00 +0300

    Task 2 complete

--- c#hw4.cs
+++ c#hw4.cs
@@ -1,23 +1,36 @@
-﻿// Задача 25: Напишите цикл, который принимает 
-// на вход два числа (A и B) и возводит число 
-// A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-int Square(int a, int b)
-{
-    int c = 0;
-    int k = 0;
-    for (int i = 0; k <= b; i++)
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+int count = 0;
+int countNum(int numA)
+{   
+    // int count = 0;
+    while (numA != 0)
     {
-        c = a*b;
-        k++;
-        Console.WriteLine(c);
+        numA = numA / 10;
+        count += 1;
     }
-
-
-    return c;
+    return count;
 }
 
+int sumNum (int numA, int lenght) 
+{
+    int length = count;
+    int result = 0;
+    for (int i = 0; i <=lenght; i++)
+    {
+        result +=  numA % 10;
+        numA = numA / 10;
+    }
+    return result;
+}
+Console.Write("Введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(countNum(82));
+Console.WriteLine($"Сумма цифр = {sumNum(x,countNum(x))}");
 
-Square(3,5);
+// Console.WriteLine();
